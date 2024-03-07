39a3f45979cfc8f8c14b8d048b578e39743c1652
AlshaheerSoft2050 <shahir4soft@gmail.com>
2024-03-08 02:27:39 +0630

    Your commit message

---
+++ spherapp14/spherapp14/doctype/orders_app/orders_app.js
@@ -0,0 +1,8 @@
+// Copyright (c) 2024, shaheer and contributors
+// For license information, please see license.txt
+
+frappe.ui.form.on('Orders App', {
+	// refresh: function(frm) {
+
+	// }
+});
---
+++ spherapp14/spherapp14/doctype/orders_app/test_orders_app.py
@@ -0,0 +1,9 @@
+# Copyright (c) 2024, shaheer and Contributors
+# See license.txt
+
+# import frappe
+from frappe.tests.utils import FrappeTestCase
+
+
+class TestOrdersApp(FrappeTestCase):
+	pass
